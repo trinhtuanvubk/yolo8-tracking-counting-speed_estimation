5a93940d78e187bef0a4e79f5e73d060334abded
trinhtuanvu <vutuantrinh2000@gmail.com>
2023-06-05 16:36:27 +0700

    x

--- predictor/speed_estimator.py
+++ predictor/speed_estimator.py
@@ -40,54 +40,6 @@ def birdeyes_speed(location1, location2):
 
 
 def transform_3d_speed(location1, location2):
-    height, width = 740, 1280
-
-    # Khởi tạo các tham số
-    pi = 3.14
-    # Góc quay camera
-    p = [-pi / 7.4, 0.11, 0.0]  # [góc quay theo trục X, quay theo trục Y, quay theo trục Z]
-
-    H = 5.0  # Chiều cao của camera so với mặt đất
-    f = 0.010  # focal_length của camera
-    # Độ rộng 1 pixel
-    pu = 0.000025
-
-    # Ma trận hướng của camera, Rx tương ứng là ma trận xoay theo trục X
-    Rx = np.array([[1, 0, 0, 0],
-                   [0, cos(-p[0]), sin(-p[0]), 0],
-                   [0, -sin(-p[0]), cos(-p[0]), 0],
-                   [0, 0, 0, 1]])
-
-    Ry = np.array([[cos(-p[1]), 0, -sin(-p[1]), 0],
-                   [0, 1, 0, 0],
-                   [sin(-p[1]), 0, cos(-p[1]), 0],
-                   [0, 0, 0, 1]])
-
-    Rz = np.array([[cos(-p[2]), sin(-p[2]), 0, 0],
-                   [-sin(-p[2]), cos(-p[2]), 0, 0],
-                   [0, 0, 1, 0],
-                   [0, 0, 0, 1]])
-
-    # Ma trận vị trí của camera
-    T = np.array([[1, 0, 0, 0],
-                  [0, 1, 0, -H],
-                  [0, 0, 1, 0],
-                  [0, 0, 0, 1]])
-    R = Rx @ Ry @ Rz
-
-    # Ma trận chiếu
-    K = np.array([[-f, 0, 0],
-                  [0, -f, 0],
-                  [0, 0, 1]])
-
-    cx = width / 2.0
-    cy = height / 2.0
-    pv = pu / width * height
-
-    # Ma trận pixel
-    P = np.array([[1 / pu, 0, cx],
-                  [0, 1 / pv, cy],
-                  [0, 0, 1]])
 
     fps = 30
     _location1 = invert(location1)
@@ -97,7 +49,7 @@ def transform_3d_speed(location1, location2):
 
     d_pixel = math.sqrt(math.pow(inverted_location2[0] - inverted_location1[0], 2) + math.pow(inverted_location2[1] - inverted_location1[1], 2))
 
-    speed = d_pixel * 30 * 3.6
+    speed = d_pixel * fps * 3.6
 
     return speed
 

513a21ea052288a5326fe9b935e766eca3aa7325
trinhtuanvu <vutuantrinh2000@gmail.com>
2023-06-05 16:13:35 +0700

    x

--- run_scenario.sh
+++ run_scenario.sh
@@ -1,9 +1,9 @@
 python3 main.py --scenario track \
 --yolo-model weights/yolov8n.pt \
 --tracking-method bytetrack \
---name test1_line \
+--speed-method transform_3d \
 --source test_data/test1.mp4 \
 --classes 0 1 2 3 5 6 7 \
+--name test1_output \
 --save \
 --save-txt \
---speed-method transform_3d
